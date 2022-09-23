f5011815ca71c5e369607ca53a7a68ebbeb9f58a
Gulzana Katkeldieva <gulzana.0185@mail.ru>
2022-09-23 01:23:01 -0500

    changing api key

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export const base_url = "https://api.fastforex.io/fetch-all?api_key=";
-export const API_key = "83716904d7-4c7534d4ce-rdgk4w";
+export const API_key = "2869f95edf-4e9b81e609-ring3v";
 export const url =
-  "https://api.fastforex.io/fetch-all?api_key=83716904d7-4c7534d4ce-rdgk4w";
+  "https://api.fastforex.io/fetch-all?api_key=2869f95edf-4e9b81e609-ring3v";
 export const USD_UAH =
-  "https://api.fastforex.io/convert?from=USD&to=UAH&amount=1.00&api_key=83716904d7-4c7534d4ce-rdgk4w";
+  "https://api.fastforex.io/convert?from=USD&to=UAH&amount=1.00&api_key=2869f95edf-4e9b81e609-ring3v";
 
 export const EUR_UAH =
-  "https://api.fastforex.io/convert?from=EUR&to=UAH&amount=1.00&api_key=83716904d7-4c7534d4ce-rdgk4w";
+  "https://api.fastforex.io/convert?from=EUR&to=UAH&amount=1.00&api_key=2869f95edf-4e9b81e609-ring3v";
